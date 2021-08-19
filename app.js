449d745321115c1689de6c45e164995de51888d1
tmak1 <tarik.khan.git@gmail.com>
2021-08-20 06:17:45 +1000

    fixed build not being served

--- app.js
+++ app.js
@@ -28,11 +28,10 @@ app.use('/api/googleApiKey', (req, res, next) => {
   res.json({ apiKey: process.env.API_KEY });
 });
 
-app.use((req, res, next) => {
-  res.status(404).json({ error: new HttpError('Could not find that route') });
-});
-
 app.use((error, req, res, next) => {
+  console.log(error.message);
+  console.log(error.code);
+
   if (req.file) {
     fs.unlink(path.resolve(__dirname, req.file.path), (err) => {
       if (err) {
@@ -44,6 +43,7 @@ app.use((error, req, res, next) => {
   if (!res.headersSent) {
     res.status(error.code).json({ error });
   }
+  next(error);
 });
 
 if (process.env.NODE_ENV === 'production') {
